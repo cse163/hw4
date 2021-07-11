6c2a52306d395d4ac21aa014af1beac27d3df63c
wenq97 <wenq97@uw.edu>
2021-07-11 15:32:58 -0700

    Added starter files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# hw4
+This repo has starter code for a CSE 163 assignment run in GitHub classroom.
 
---
+++ hw4_test.py
@@ -0,0 +1,14 @@
+from cse163_utils import assert_equals
+
+from document import Document
+from search_engine import SearchEngine
+
+
+# Define your test functions here!
+
+def main():
+    # Call your test functions here!
+
+
+if __name__ == '__main__':
+    main()
